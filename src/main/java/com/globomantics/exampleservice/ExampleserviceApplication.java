f8d9067425598972d36a34ebd202eadabd4ebf4e
Eugene Sia <eugene@eugenesia.co.uk>
2019-10-14 15:10:39 +0100

    Initial commit

---
+++ src/main/java/com/globomantics/exampleservice/ExampleserviceApplication.java
@@ -0,0 +1,13 @@
+package com.globomantics.exampleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleserviceApplication.class, args);
+	}
+
+}
